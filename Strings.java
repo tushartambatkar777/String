bc3909c0c03bbc484826fca16d1739f2e6830c43
tushartambatkar <tushartambatkar777@gmail.com>
2024-05-03 10:51:13 +0530

    code commit

--- Strings.java
+++ Strings.java
@@ -2,30 +2,30 @@ import java.util.*;
 
 public class Strings {
   public static void main(String args[]) {
-    /
-     * // Declaration
-     * String myName = "Tushar";
-     * System.out.println(myName);
-     * 
-     * //Taking Input
-     * Scanner sc = new Scanner(System.in);
-     * String name = sc.nextLine();
-     * 
-     * 
-     * //concatenation [Joining two String]
-     * String firstName = "Tony";
-     * String lastName = "Stark";
-     * String fullName = firstName + " "+ lastName;
-     * System.out.println(fullName);
-     * 
-     * //length
-     * System.out.println(fullName.length());
-     * 
-     * 
-     * //Access charecters of a String
-     * String name = "Jacqueline";
-     * System.out.print(name.charAt(3));
-     *
+/* 
+    // Declaration
+    String myName = "Tushar";
+    System.out.println(myName);
+
+    //Taking Input
+    Scanner sc = new Scanner(System.in);
+    String name = sc.nextLine();
+
+
+    //concatenation [Joining two String]
+    String firstName = "Tony";
+    String lastName = "Stark";
+    String fullName = firstName + " "+ lastName;
+    System.out.println(fullName);
+
+    //length
+    System.out.println(fullName.length());
+
+
+    //Access charecters of a String
+    String name = "Jacqueline";
+    System.out.print(name.charAt(3));
+*/
 
     // String function compare
     String s1 = "Tony";
