239fc5b8b22266a9e63fabb27c251eeb09781e04
tushartambatkar <tushartambatkar777@gmail.com>
2024-05-03 10:47:06 +0530

    code commit

--- Strings.java
+++ Strings.java
@@ -1,29 +1,52 @@
 import java.util.*;
 
-public class Strings{
+public class Strings {
   public static void main(String args[]) {
-/* 
-    // Declaration
-    String myName = "Tushar";
-    System.out.println(myName);
+    /
+     * // Declaration
+     * String myName = "Tushar";
+     * System.out.println(myName);
+     * 
+     * //Taking Input
+     * Scanner sc = new Scanner(System.in);
+     * String name = sc.nextLine();
+     * 
+     * 
+     * //concatenation [Joining two String]
+     * String firstName = "Tony";
+     * String lastName = "Stark";
+     * String fullName = firstName + " "+ lastName;
+     * System.out.println(fullName);
+     * 
+     * //length
+     * System.out.println(fullName.length());
+     * 
+     * 
+     * //Access charecters of a String
+     * String name = "Jacqueline";
+     * System.out.print(name.charAt(3));
+     *
 
-    //Taking Input
-    Scanner sc = new Scanner(System.in);
-    String name = sc.nextLine();
+    // String function compare
+    String s1 = "Tony";
+    String s2 = "Tony";
+    String s3 = new String("Tony");
 
+    if (s1 == s2) {
+      System.out.println("Strings are equal");
+    } else {
+      System.out.println("Strings are not equal");
+    }
 
-    //concatenation [Joining two String]
-    String firstName = "Tony";
-    String lastName = "Stark";
-    String fullName = firstName + " "+ lastName;
-    System.out.println(fullName);
+    // Equals
+    if (s1.equals(s3)) {
+      System.out.println("Strings are equal");
+    } else {
+      System.out.println("Strings are not equal");
+    }
 
-    //length
-    System.out.println(fullName.length());
-    */
-
-    //Access charecters of a String
-    String name = "Jacqueline";
-    System.out.print(name.charAt(3));
+    // subString
+    String str = "HelloWorld";
+    System.out.println(str.substring(0, 5));
   }
 }
